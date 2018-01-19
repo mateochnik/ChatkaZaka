57b8545fd27fd49c7274de07c444c0771226485a
mateochnik <mateusz.ochnik@gmail.com>
2018-01-19 17:47:11 +0100

    coomiit

--- app/src/main/java/com/example/mateo/chatkazaka/Pomaran.java
+++ app/src/main/java/com/example/mateo/chatkazaka/Pomaran.java
@@ -1,17 +1,20 @@
 package com.example.mateo.chatkazaka;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by Mateo on 17.01.2018.
  */
 public class Pomaran extends Activity {
+    Context context;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,19 +34,20 @@ public class Pomaran extends Activity {
         };
         ListView prosta_lista = (ListView) findViewById(R.id.lista);
         List_Custom_List adapter_listy = new List_Custom_List(this, elementy_listy);
-
+        context = getApplicationContext();
         prosta_lista.setAdapter(adapter_listy);;
         prosta_lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             public void onItemClick(AdapterView arg0, View arg1, int pos,
                                     long arg3) {
+                if(pos == 0)
+                {
+                    Intent startActivityCustomList = new Intent(Pomaran.this,
+                            wyda_pom.class);
+                    startActivity(startActivityCustomList);
 
-                switch (pos) {
-                    case 0:
-                        Intent startActivityCustomList = new Intent(Pomaran.this,
-                                wyda_pom.class);
-                        startActivity(startActivityCustomList);
-                        break;
+                } else if (pos>0) {
+                    Toast.makeText(context,"Dostępne wkrótce......",Toast.LENGTH_LONG ).show();
                 }
 
             }
